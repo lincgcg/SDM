0396d7ef5af9754d07232d6ea0bddd31f0091ab5
“lincgcg <chunganglin@gmail.com>
2023-09-08 22:59:07 +0800

    de

--- networks/__init__.py
+++ networks/__init__.py
@@ -2,5 +2,5 @@ from . import *
 
 __all__=[
     'FPMMs','FRPMMs',
-    'FPMMs_vgg','FRPMMs_vgg','panet','resnet50_34','R_pmm','FPMMs_one','canet','PFENet'
+    'FPMMs_vgg','FRPMMs_vgg','panet','R_pmm','FPMMs_one','canet','PFENet'
 ]
